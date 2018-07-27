649194ac08d807debc8dcdbceccc984770e3f963
lcs_global <2951215162@qq.com>
2018-07-27 16:32:52 +0800

    linux commit

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "hello Word !"
+
+yourname="lichangsheng"
+
+echo $yourname
+#==============
+for skill in 'ada coffe Action java'; 
+do
+	echo "l am good at ${skill} Script"
+done
+
+yourname="li"
+
+echo $yourname
+
+
